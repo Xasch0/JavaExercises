b4b371b92aa3ddabf56d0c7e68672a2442e22f3f
Bernardo Xavier <bernardovxavier@gmail.com>
2022-02-14 10:01:32 -0300

    b

---
+++ exercises/DaysOfTheWeek.java
@@ -0,0 +1,26 @@
+package com.xavier.JavaTraining.exercises;
+
+
+import java.util.Scanner;
+
+public class DaysOfTheWeek {
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Input the weekday(as a number): ");
+        int weekDay = input.nextInt();
+        input.close();
+        String weekDayString;
+        switch (weekDay) {
+            case 1 -> weekDayString = "Monday";
+            case 2 -> weekDayString = "Tuesday";
+            case 3 -> weekDayString = "Wednesday";
+            case 4 -> weekDayString = "Thursday";
+            case 5 -> weekDayString = "Friday";
+            case 6 -> weekDayString = "Saturday";
+            case 7 -> weekDayString = "Sunday";
+            default -> weekDayString = "Invalid weekday";
+        }
+        System.out.println(weekDayString);
+    }
+}
